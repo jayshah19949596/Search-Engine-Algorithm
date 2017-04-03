9a25b3b787bc0c672289910fa8be56733eefab0d
Jai Shah <jai.s@somaiya.edu>
2017-04-03 17:33:54 -0500

    Update SearchEngine.py

--- SearchEngine.py
+++ SearchEngine.py
@@ -16,9 +16,7 @@ def crawl_web(seed):  # returns index, graph of outlinks
 			content = get_page(page)
 			add_page_to_index(index, page, content)
 			outlinks = get_all_links(content)
-			graph[page] = outlinks
-			# Insert Code Here
-			
+			graph[page] = outlinks		
 			union(tocrawl, outlinks)
 			crawled.append(page)
 	return index, graph
